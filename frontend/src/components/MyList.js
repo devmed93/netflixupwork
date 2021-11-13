2c3ebc20637827ff9b777953aa7c351aaedf9f6b
El-Moutaouakkil <el.moutaouakkil@hotmail.com>
2021-11-13 01:58:31 +0100

    add movie appears instantly on myList UI Fixed

--- frontend/src/components/MyList.js
+++ frontend/src/components/MyList.js
@@ -1,32 +1,29 @@
+import axios from "axios";
+import useAxios from "axios-hooks";
 import React, { useEffect, useState } from "react";
-import {
-    addMovieToMyList,
-    selectmyMovieList,
-} from "../features/myMovieListSlice";
+import { Link, useHistory } from "react-router-dom";
+import Spinner from "react-spinkit";
 import "./MyList.scss";
 import Nav from "./Nav";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-
-import { Link, useHistory } from "react-router-dom";
-import useAxios from "axios-hooks";
-import Spinner from "react-spinkit";
 
 function MyList() {
     const emptyListMessage = <Link to='/'>Add movies from the HomeScreen</Link>;
     const [myList, setMyList] = useState([]);
-    const [{ data, loading, error }, refetch] = useAxios(
-        "http://localhost:5000/movies/list"
-    );
+    // const [{ data, loading, error }, refetch] = useAxios(
+    //     "http://localhost:5000/movies/list"
+    // );
+    const [loading, setLoading] = useState(true);
     const base_url = "https://image.tmdb.org/t/p/w300";
-    const dispatch = useDispatch();
     const history = useHistory();
+
     console.count("mylist render");
-  
+
     useEffect(() => {
-        setMyList(data);
-        
-    }, [data]);
+        axios.get("http://localhost:5000/movies/list").then((response) => {
+            setMyList(response.data);
+            setLoading(false);
+        });
+    }, []);
 
     return (
         <div className='myList'>
@@ -47,7 +44,7 @@ function MyList() {
                 <div className='myList-posters'>
                     {myList?.map(
                         (movie) =>
-                            (movie.poster_path || movie.backdrop_path) && (
+                            (movie?.poster_path || movie?.backdrop_path) && (
                                 <div
                                     className='poster-container'
                                     key={movie.id}>
